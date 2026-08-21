f3c830954f3e8ddb0b0975e88cef17c8bb675a23
Sacha Schmid <sacha.schmid@proton.me>
2026-08-21 12:56:02 +0200

    feat: update fonts (#6)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { unified } from '@astrojs/markdown-remark';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
@@ -11,6 +11,35 @@ export default defineConfig({
   },
   integrations: [sitemap()],
   trailingSlash: 'always',
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Nunito',
+      cssVariable: '--font-nunito',
+      weights: [400, 700],
+      fallbacks: [
+        'Avenir Next',
+        'Avenir',
+        'Helvetica Neue',
+        'Arial',
+        'sans-serif',
+      ],
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Montserrat',
+      cssVariable: '--font-montserrat',
+      weights: [700],
+      styles: ['normal'],
+      fallbacks: [
+        'Avenir Next',
+        'Avenir',
+        'Helvetica Neue',
+        'Arial',
+        'sans-serif',
+      ],
+    },
+  ],
   markdown: {
     processor: unified({
       rehypePlugins: [
